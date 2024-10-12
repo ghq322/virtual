000c5c3e635fb0cc562a38002dcf2bd1fb1bb705
website-life <384754419@qq.com>
2024-10-12 21:22:22 +0800

    featch:first

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -3,21 +3,11 @@ import Image from "next/image";
 
 import { Button,Form,Input } from 'antd-mobile'
 import Link from "next/link";
-export default function Home() {
+export default function Game() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <Form layout='horizontal'>
-          <Form.Item label='用户名' name='username'>
-            <Input placeholder='请输入用户名' clearable />
-          </Form.Item>
-         
-        </Form>
-        <Link href="/game">
-        <Button block color='primary' size='large'>
-         登录
-        </Button>
-        </Link>
+      4
       </main>
       
     </div>
